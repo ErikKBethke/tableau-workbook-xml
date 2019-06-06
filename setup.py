78e78e1c795ef62727756da927ceeee0bfd9d53c
EBethke <erik.k.bethke@gmail.com>
2019-06-06 11:31:53 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='tableau-workbook-xml',
+setup(name='tableauxml',
       version='0.1',
       description='Python manipulation of Tableau workbooks via the XML',
       url='https://github.com/ErikKBethke/tableau-workbook-xml',
       author='ErikKBethke',
       author_email='erik.k.bethke@gmail.com',
       license='MIT',
-      packages=['tableau-workbook-xml'],
+      packages=['tableauxml'],
       install_requires=[
       'lxml'
     ]

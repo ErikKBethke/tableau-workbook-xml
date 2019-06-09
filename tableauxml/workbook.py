e63a763675b22aa4e571c8c39294ceffa9953407
EBethke <erik.k.bethke@gmail.com>
2019-06-09 14:38:41 -0400

    workbook class creation

--- tableauxml/workbook.py
+++ tableauxml/workbook.py
@@ -1,2 +1,36 @@
-def test():
-    return (u'test functionality')
+import lxml.etree as etree
+from io import StringIO, BytesIO
+
+# %%
+class Workbook:
+    """
+    Creates a Tableau Workbook object.
+    """
+
+    def __init__(self, filepath, encoding='utf-8'):
+        self.filepath = filepath
+        self.encoding = encoding
+        self.xml = None
+
+    def load_xml(self):
+        """
+        Takes the workbook filepath and creates an ElementTree object for xml.
+        """
+        self.xml = etree.parse(self.filepath)
+
+    def get_docinfo(self):
+        """
+        Returns all doc info values for the XML object.
+        """
+        return {
+                'URL' : self.xml.docinfo.URL,
+                'doctype' : self.xml.docinfo.doctype,
+                'encoding' : self.xml.docinfo.encoding,
+                'externalDTD' : self.xml.docinfo.externalDTD,
+                'internalDTD' : self.xml.docinfo.externalDTD,
+                'public_id' : self.xml.docinfo.public_id,
+                'root_name' : self.xml.docinfo.root_name,
+                'standalone' : self.xml.docinfo.standalone,
+                'system_url' : self.xml.docinfo.system_url,
+                'xml_version' : self.xml.docinfo.xml_version,
+                }
